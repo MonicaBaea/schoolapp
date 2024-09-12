88c33be73524dd8e02e664f40711c9a4ec3b0fb4
Monica Araujo <108374729+MonicaMajete@users.noreply.github.com>
2024-09-12 20:53:50 -0300

    INITIAL CAS APP GOOD

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,4 +1,4 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
+// import { GlobeAltIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 
 export default function AcmeLogo() {
@@ -6,8 +6,8 @@ export default function AcmeLogo() {
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+    
+      <p className="text-[44px]">CAS Opps</p>
     </div>
   );
 }
